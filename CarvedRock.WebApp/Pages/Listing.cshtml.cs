2167f17c59a98dd967a9c0379cebf098bbb42cac
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-16 01:09:47 +0300

    User Information

--- CarvedRock.WebApp/Pages/Listing.cshtml.cs
+++ CarvedRock.WebApp/Pages/Listing.cshtml.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Headers;
 using CarvedRock.WebApp.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,32 +10,42 @@ namespace CarvedRock.WebApp.Pages
     {
         private readonly HttpClient _apiClient;
         private readonly ILogger<ListingModel> _logger;
+        private readonly HttpContext? _httpContext;
 
-        public ListingModel(HttpClient apiClient,ILogger<ListingModel> logger)
+        public ListingModel(HttpClient apiClient, ILogger<ListingModel> logger, IHttpContextAccessor httpContextAccessor)
         {
             _apiClient = apiClient;
             _apiClient.BaseAddress = new Uri("https://localhost:7213/");
             _logger = logger;
+            _httpContext = httpContextAccessor.HttpContext;
         }
 
         public List<Product> Products { get; set; }
         public string CategoryName { get; set; } = "";
 
         public async Task OnGetAsync()
-        { 
+        {
             var cat = Request.Query["cat"].ToString();
             if (string.IsNullOrEmpty(cat))
             {
                 throw new Exception("failed");
             }
 
+            if (_httpContext != null)
+            {
+                var accessToken = await _httpContext.GetTokenAsync("access_token");
+                _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            }
+
             var response = await _apiClient.GetAsync($"Product?category={cat}");
             if (!response.IsSuccessStatusCode)
             {
                 var fullPath = $"{_apiClient.BaseAddress}Product?category={cat}";
-                var details = await response.Content.ReadFromJsonAsync<ProblemDetails>()??new ProblemDetails();
-                var traceId= details.Extensions["traceId"]?.ToString();
-                _logger.LogWarning("API failure: {FullPath} Response: {ResponseStatusCode}, Trace: {Trace}", fullPath, (int)response.StatusCode, traceId);
+                var details = await response.Content.ReadFromJsonAsync<ProblemDetails>() ?? new ProblemDetails();
+                var traceId = details.Extensions["traceId"]?.ToString();
+
+                var userName = User.Identity?.IsAuthenticated ?? false ? User.Identity.Name : "";
+                _logger.LogWarning("API failure: {fullPath} Response: {apiResponse}, Trace: {trace}, User: {user}", fullPath, (int)response.StatusCode, traceId, userName);
                 throw new Exception("API call failed!");
             }
 
--- CarvedRock.WebApp/Program.cs
+++ CarvedRock.WebApp/Program.cs
@@ -1,7 +1,35 @@
 using Microsoft.AspNetCore.HttpLogging;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://demo.duendesoftware.com";
+        options.ClientId = "interactive.confidential";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+        options.Scope.Add("api");
+        options.Scope.Add("offline_access");
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "email"
+        };
+        options.SaveTokens = true;
+    });
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpLogging(options =>
 {
     options.LoggingFields = HttpLoggingFields.All;
@@ -38,11 +66,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapRazorPages().RequireAuthorization();
 
 app.Run();
