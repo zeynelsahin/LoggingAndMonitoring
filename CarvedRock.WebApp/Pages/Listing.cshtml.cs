c5ea90bbdc517274c0e3ef8a1b9b0b884b54985c
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-12-16 00:06:03 +0300

    HTTP and W3C Logging

--- CarvedRock.WebApp/Pages/Listing.cshtml.cs
+++ CarvedRock.WebApp/Pages/Listing.cshtml.cs
@@ -1,4 +1,5 @@
 using CarvedRock.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CarvedRock.WebApp.Pages
@@ -6,18 +7,20 @@ namespace CarvedRock.WebApp.Pages
     public class ListingModel : PageModel
     {
         private readonly HttpClient _apiClient;
+        private readonly ILogger<ListingModel> _logger;
 
-        public ListingModel(HttpClient apiClient)
+        public ListingModel(HttpClient apiClient,ILogger<ListingModel> logger)
         {
             _apiClient = apiClient;
             _apiClient.BaseAddress = new Uri("https://localhost:7213/");
+            _logger = logger;
         }
 
         public List<Product> Products { get; set; }
         public string CategoryName { get; set; } = "";
 
         public async Task OnGetAsync()
-        {
+        { 
             var cat = Request.Query["cat"].ToString();
             if (string.IsNullOrEmpty(cat))
             {
@@ -27,6 +30,10 @@ namespace CarvedRock.WebApp.Pages
             var response = await _apiClient.GetAsync($"Product?category={cat}");
             if (!response.IsSuccessStatusCode)
             {
+                var fullPath = $"{_apiClient.BaseAddress}Product?category={cat}";
+                var details = await response.Content.ReadFromJsonAsync<ProblemDetails>()??new ProblemDetails();
+                var traceId= details.Extensions["traceId"]?.ToString();
+                _logger.LogWarning("API failure: {FullPath} Response: {ResponseStatusCode}, Trace: {Trace}", fullPath, (int)response.StatusCode, traceId);
                 throw new Exception("API call failed!");
             }
 
--- CarvedRock.WebApp/Program.cs
+++ CarvedRock.WebApp/Program.cs
@@ -1,11 +1,33 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.MediaTypeOptions.AddText("application/json");
+    options.RequestBodyLogLimit = 4096;
+    options.RequestBodyLogLimit = 4096;
+});
+
+var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+builder.Services.AddW3CLogging(options =>
+{
+    options.LoggingFields = W3CLoggingFields.All;
+    options.FileSizeLimit = 5 * 1024 * 1024;
+    options.RetainedFileCountLimit = 2;
+    options.FileName = "CarvedRock-W3C-UI";
+    options.LogDirectory = Path.Combine(path, "logs");
+    options.FlushInterval = TimeSpan.FromSeconds(2);
+});
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+app.UseHttpLogging();
+app.UseW3CLogging();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
